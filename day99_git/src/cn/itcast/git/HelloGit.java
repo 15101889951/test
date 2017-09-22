0f7cb3692f5901aa060b42214b2b5318337abc76
maven <taoge_email@163.com>
2017-09-22 16:49:42 +0800

    修改

--- day99_git/src/cn/itcast/git/HelloGit.java
+++ day99_git/src/cn/itcast/git/HelloGit.java
@@ -4,5 +4,13 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.err.println("hello git");
+		System.err.println("hello git");
+		System.err.println("hello git");
+		System.err.println("hello git");
+		System.err.println("hello git");
+		System.err.println("hello git");
+		System.err.println("hello git");
+		System.err.println("hello git");
+		System.err.println("hello git");
 	}
 }
